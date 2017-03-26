c4379fe087c2cdb7a6d6ff54fa9affeb7cdfda37
sbandari <bsantosh027@gmail.com>
2017-03-25 23:09:11 -0500

    first Commit

---
+++ Memory_Game/app/src/main/java/com/packtpub/memory_game/GameActivity.java
@@ -0,0 +1,14 @@
+package com.packtpub.memory_game;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
